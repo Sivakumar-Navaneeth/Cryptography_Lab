aeefed7872f311ea4856133ec27c3bd37716a73a
Sivakumar-Navaneeth <sivakumar.navaneeth66@gmail.com>
2024-08-04 20:46:15 +0530

    MODIFY Lab-1

--- Lab-1/caeser.py
+++ Lab-1/caeser.py
@@ -1,17 +1,38 @@
-def Ceaser(text):
+def Substitution(text,key):
+    """Encrypts the given text using the Ceaser Cipher with the specified key."""
     cipher = ""
     for i in text:
         if i.isspace():
             cipher += " "
         elif i.isalpha():
             c = ord(i) -65
-            cipher += chr((c+3)%26 + 65)
+            cipher += chr((c+key)%26 + 65)
         elif i.isalnum():
-            c = (int(i) + 3)%10
+            c = (int(i) + key)%10
             cipher += str(c)
         else:
             cipher += i
     print("Cipher is: ",cipher)
+    return cipher
+
+def DecryptSubstitution(cipher, key):
+    """Encrypts the given text using the Ceaser Cipher with the specified key."""
+    plaintext = ""
+    for i in cipher:
+        if i.isspace():
+            plaintext += " "
+        elif i.isalpha():
+            c = ord(i) - 65
+            plaintext += chr((c - key) % 26 + 65)
+        elif i.isalnum():
+            c = (int(i) - key) % 10
+            plaintext += str(c)
+        else:
+            plaintext += i
+    print("Plaintext is: ", plaintext)
+    return plaintext
 
 plaintext = input("Enter PlainText: ")
-Ceaser(plaintext.upper())
+key = int(input("Enter Key: "))
+ciphertext = Substitution(plaintext.upper(),key)
+DecryptSubstitution(ciphertext,key)
---
+++ Lab-1/railfence.py
@@ -0,0 +1,61 @@
+def encrypt_rail_fence(text, key):
+    """Encrypts the given text using the Rail Fence Cipher with the specified key."""
+
+    rail = [[] for _ in range(key)]
+    direction = 1
+    row = 0
+
+    for char in text:
+        rail[row].append(char)
+        if row == 0:
+            direction = 1
+        elif row == key - 1:
+            direction = -1
+        row += direction
+
+    ciphertext = ''.join(''.join(row) for row in rail)
+    return ciphertext
+
+def decrypt_rail_fence(ciphertext, key):
+    """Decrypts the given ciphertext using the Rail Fence Cipher with the specified key."""
+
+    rail = [[] for _ in range(key)]
+    direction = 1
+    row = 0
+
+    for _ in ciphertext:
+        rail[row].append('*')
+        if row == 0:
+            direction = 1
+        elif row == key - 1:
+            direction = -1
+        row += direction
+
+    index = 0
+    for i in range(key):
+        for j in range(len(rail[i])):
+            if rail[i][j] == '*':
+                rail[i][j] = ciphertext[index]
+                index += 1
+
+    plaintext = ''
+    row = 0
+    direction = 1
+    for _ in ciphertext:
+        plaintext += rail[row].pop(0)
+        if row == 0:
+            direction = 1
+        elif row == key - 1:
+            direction = -1
+        row += direction
+
+    return plaintext
+
+plaintext = input("Enter PlainText: ")
+key = int(input("Enter Key: "))
+
+encrypted_text = encrypt_rail_fence(plaintext, key)
+print("Encrypted text:", encrypted_text)
+
+decrypted_text = decrypt_rail_fence(encrypted_text, key)
+print("Decrypted text:", decrypted_text)
